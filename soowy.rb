563e812bdfcd7868eee61c0ee42deb347f853f51
Paul McCulloch <hrfeels@gmail.com>
2014-08-14 20:56:54 -0700

    cleanup

--- soowy.rb
+++ soowy.rb
@@ -1,42 +1,32 @@
 #!/usr/bin/env ruby
 
-# TODO 1 replace SPLIT_ON with regex, allow user to provide
 # TODO rescue exceptions and ARGV.delete file
-# TODO option parser
 # TODO trap SIG_INT and print results so far, then exit
 # TODO check:
   # does array subtraction benefit from sorted?
   # non-in-place ary methods faster for short-line low-memory case?
 # TODO may need to chunk if run on single-line files, eg compiled css
 require 'optparse'
-
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: example.rb [options]"
-
   options[:regex] = ""
   options[:files] = []
   options[:verbose] = false
-  opts.banner =
-"""
-Usage:
-ruby soowy.rb file file..
-or pipe command:
-find path/to/scss -type f -name test\\*.txt | xargs ruby soowy.rb
-"""
-
-  opts.on("-r", "--regex /REGEX/", "regex used to match candidates") do |regex|
+  opts.banner = """
+  Usage:
+  ruby soowy.rb -v -r regex -f file,file..
+  or pipe command:
+  find path/to/scss -type f -name test\\*.txt | xargs ruby soowy.rb -r regex -f
+  """
+  opts.on("-r", "--regex /my_regex/", "regex used to match candidates") do |regex|
     options[:regex] << regex
   end
-
   opts.on("-f", "--files file1,file2 ..", Array, "file list to search") do |files|
     options[:files] = files
   end
-
-  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+  opts.on("-v", "--[no-]verbose", "run verbosely") do |v|
     options[:verbose] = v
   end
-
 end.parse!
 
 unique = []
