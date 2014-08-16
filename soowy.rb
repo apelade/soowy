5646e3f024a0ba9123355107ee3f6c5369f1100b
Paul McCulloch <hrfeels@gmail.com>
2014-08-15 23:37:08 -0700

    fixed verbose, still have to give regex as string

--- soowy.rb
+++ soowy.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# TODO 1 allow giving unquoted regex
 # TODO map reduce
 # TODO rescue exceptions and report sep category for unavailable files?
 # TODO trap SIG_INT and print results so far, then exit
@@ -6,26 +7,25 @@
 # does array subtraction benefit from sorted?
 # non-in-place ary methods faster for short-line low-memory case?
 # TODO may need to chunk if run on single-line files, eg compiled css
-usage = "" "
-  Usage:
-  ruby soowy.rb -r regex -f file,file,..
-  Example:
-  ruby soowy.rb -r \"\w.*\w\" -f test1.txt,test2.txt
-  or pipe command:
-  find path/to/scss -type f -name test\\*.scss | xargs ruby soowy.rb -r \"/\./\w\" -f
-  " ""
-fail(usage) if ARGV.empty?
+
+usage = """
+Usage:
+  ruby soowy.rb -r \'inner_regex\' -f file file --verbose
+Example:
+  ruby soowy.rb -r \'\\w.*\\w\' -f test1.txt test2.txt -v
+Example piping from shell command:
+  find . -type f -name test\\*.txt | xargs ruby soowy.rb -r '\\w.*\\w' -f
+  """
 
 require 'optparse'
+require 'to_regexp'
+require 'byebug'
 options = {}
-OptionParser.new do |opts|
-
-  DEFAULT_REGEX = %r{\w.*\w}
+OptionParser.new(usage) do |opts|
+  DEFAULT_REGEX = /\w.*\w/
   options[:regex] = DEFAULT_REGEX
-  options[:files] = []
   options[:verbose] = false
-  opts.banner = usage
-  opts.on("-r", "--regex \"#{options[:regex]}\"", "regex used to match candidates") do |regex|
+  opts.on("-r", "--regex \'\\w.*\\w\'", "single-quoted inner string for scan regex") do |regex|
     begin
       regex = Regexp.new(regex) unless regex.instance_of? Regexp
       options[:regex] = regex
@@ -33,17 +33,20 @@ OptionParser.new do |opts|
       puts "Error creating regular expression from #{regex}"
     end
   end
-  opts.on("-f", "--files file1,file2 ..", Array, "file list to search") do |files|
-    options[:files] = files
-  end
   opts.on("-v", "--[no-]verbose", "run verbosely") do |v|
     options[:verbose] = v
   end
+  opts.banner = usage
+
+  opts.parse!
+  # anything left over is assumed to be a file name
+  options[:files] = ARGV
+  puts opts if options[:files].empty?
 end.parse!
 using_default = "Using Default " if options[:regex] == DEFAULT_REGEX
-puts using_default.to_s + "Regex : #{options[:regex]}" if using_default || options[:verbose]
-unique = []
+puts using_default.to_s + "Regex : #{options[:regex].to_s}" if using_default || options[:verbose]
 
+unique = []
 # find unique words from each file
 options[:files].each do |file|
   raise "file not found: ${file}" unless File.exists? file
