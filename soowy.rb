80f9d952464e7c37d88aa5c5b8fbac36a2df06a1
Paul McCulloch <hrfeels@gmail.com>
2014-08-16 20:17:59 -0700

    make it a class, can use either single or double quotes for regex innards

--- soowy.rb
+++ soowy.rb
@@ -1,69 +1,83 @@
 #!/usr/bin/env ruby
-# TODO map reduce
-# TODO rescue exceptions and report sep category for unavailable files?
-# TODO trap SIG_INT and print results so far, then exit
 # TODO check:
 # does array subtraction benefit from sorted?
 # non-in-place ary methods faster for short-line low-memory case?
-# TODO may need to chunk if run on single-line files, eg compiled css
-
-usage = """
-Usage: Find strings that occur only once among a set of files
-  ruby soowy.rb -r \'inner_regex\' file file --verbose
-Example:
-  ruby soowy.rb -r \'\\w.*\\w\' test1.txt test2.txt -v
-Example piping from shell command:
-  find ../websiteone/app/assets/stylesheets/ -type f -name *.scss | xargs ruby soowy.rb -r '\\A\\.\\w.*\\w {' -v
-"""
+# need to chunk if run on single-line files, eg compiled css
 
 require 'optparse'
-options = {}
-OptionParser.new(usage) do |opts|
+
+class Soowy
   DEFAULT_REGEX = /\w.*\w/
-  options[:regex] = DEFAULT_REGEX
-  options[:verbose] = false
-  opts.on("-r", "--regex \'\\w.*\\w\'", "single-quoted inner string for scan regex") do |regex|
-    begin
-      regex = Regexp.new(regex) unless regex.instance_of? Regexp
-      options[:regex] = regex
-    rescue RegexpError
-      puts "Error creating regular expression from #{regex}"
-      exit! 1
+
+  def self.usage
+    "" "
+  Usage: Find strings that occur only once in a set of files
+    ruby soowy.rb -r \'inner_regex\' file file --verbose
+  Example:
+    ruby soowy.rb -r \'\\w.*\\w\' test1.txt test2.txt -v
+  Example piping from shell command:
+    find ../websiteone/app/assets/stylesheets/ -type f -name *.scss | xargs ruby soowy.rb -r '\\A\\.\\w.*\\w {'
+  " ""
+  end
+
+  def self.parse_opts(usage)
+    options = {}
+    OptionParser.new(usage) do |opts|
+      options[:regex] = ''
+      options[:verbose] = false
+      opts.on("-r", "--regex \'\\w.*\\w\'", "quoted inner string for scan regex") do |regex|
+        begin
+          regex = Regexp.new(regex) unless regex.instance_of? Regexp
+          options[:regex] = regex
+        rescue RegexpError => e
+          raise "Error creating regular expression from #{regex} " + e.inspect
+        end
+      end
+      opts.on("-v", "--[no-]verbose", "run verbosely") do |v|
+        options[:verbose] = v
+      end
+      opts.parse!
+      options[:files] = ARGV
+      fail opts.to_s if options[:files].empty? && options[:regex] == ''
+      options[:regex] = DEFAULT_REGEX if options[:regex] == ''
+    end.parse!
+    using_default = "Using Default " if options[:regex] == DEFAULT_REGEX
+    puts using_default.to_s + "Regex : #{options[:regex].to_s}" if using_default || options[:verbose] || options[:files].empty?
+    options
+  end
+
+  def self.find_unique(regex, files, verbose)
+    unique = []
+    files.each do |file|
+      raise "file not found: ${file}" unless File.exists? file
+      raise "file not readable: ${file}" unless File.readable? file
+      singles = []
+      open(file) do |text|
+        text.each_line do |line|
+          words = line.scan regex
+          # XOR arrays
+          singles = (singles - words) + (words - singles)
+          singles.sort!
+        end
+        puts "result for file #{file}:", singles.flatten if verbose
+      end
+      unique = (unique - singles) + (singles - unique)
+      unique.sort!
+    end
+    unique
+  end
+
+  def self.format_result(unique, files, verbose)
+    if verbose
+      "unique across files #{files.join(" ")} : #{unique}"
+    else
+      unique
     end
   end
-  opts.on("-v", "--[no-]verbose", "run verbosely") do |v|
-    options[:verbose] = v
-  end
-  opts.parse!
-  # anything left over is assumed to be a file name
-  options[:files] = ARGV
-  fail opts.to_s if options[:files].empty?
-end.parse!
-using_default = "Using Default " if options[:regex] == DEFAULT_REGEX
-puts using_default.to_s + "Regex : #{options[:regex].to_s}" if using_default || options[:verbose]
 
-# find unique words from each file
-unique = []
-options[:files].each do |file|
-  raise "file not found: ${file}" unless File.exists? file
-  raise "file not readable: ${file}" unless File.readable? file
-  singles = []
-  open(file) do |text|
-    text.each_line do |line|
-      words = line.scan options[:regex]
-      # XOR arrays
-      singles = (singles - words) + (words - singles)
-      singles.sort!
-    end
-    puts "result for file #{file}:", singles.flatten if options[:verbose]
-  end
-  unique = (unique - singles) + (singles - unique)
-  unique.sort!
-end
+end # end class Soowy
 
-if options[:verbose]
-  puts "unique across files #{options[:files].join(",")} : #{unique}"
-else
-  puts unique
-end
+options = Soowy.parse_opts(Soowy.usage())
+unique  = Soowy.find_unique(options[:regex], options[:files], options[:verbose])
+puts Soowy.format_result(unique, options[:files], options[:verbose])
 
